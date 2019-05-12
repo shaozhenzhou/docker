025a5640d903eb81ca9dc6fd695320a40a9f061b
shaozhenzhou <46422110+shaozhenzhou@users.noreply.github.com>
2019-05-12 20:31:30 +0800

    Add files via upload

---
+++ libfacedetection/aarch64-toolchain.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+set(CMAKE_CXX_COMPILER "/usr/bin/arm-linux-gnueabihf-g++")
+set(CMAKE_C_COMPILER "/usr/bin/arm-linux-gnueabihf-gcc")
+
